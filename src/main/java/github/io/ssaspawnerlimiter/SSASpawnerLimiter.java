cda70c6669283c38ceb9bb9412eeeaa5e386c1e5
Tien Thanh <notnighter@gmail.com>
2025-11-16 20:54:57 +0700

    update: enhance plugin functionality and versioning

--- src/main/java/github/io/ssaspawnerlimiter/SSASpawnerLimiter.java
+++ src/main/java/github/io/ssaspawnerlimiter/SSASpawnerLimiter.java
@@ -6,23 +6,56 @@ import org.bukkit.plugin.java.JavaPlugin;
 import io.github.pluginlangcore.language.LanguageManager;
 import io.github.pluginlangcore.language.MessageService;
 import io.github.pluginlangcore.updater.LanguageUpdater;
+import io.github.pluginupdatecore.updater.UpdateChecker;
+import io.github.pluginupdatecore.updater.ConfigUpdater;
+import github.nighter.smartspawner.api.SmartSpawnerAPI;
+import github.nighter.smartspawner.api.SmartSpawnerProvider;
 
 import java.util.List;
 
 @Getter
 @Accessors(chain = false)
 public final class SSASpawnerLimiter extends JavaPlugin {
+    private final static String MODRINTH_PROJECT_ID = "";
     private LanguageManager languageManager;
     private MessageService messageService;
+    private SmartSpawnerAPI api;
 
-    @Override
-    public void onEnable() {
+    private void checkSmartSpawnerAPI() {
+        api = SmartSpawnerProvider.getAPI();
+        if (api == null) {
+            getLogger().warning("SmartSpawner not found!");
+        } else {
+            getLogger().info("SmartSpawner found, SSA Spawner Limiter is operational!");
+        }
+    }
+
+    private void checkPluginUpdates() {
+        UpdateChecker updateChecker = new UpdateChecker(this, MODRINTH_PROJECT_ID);
+        updateChecker.checkForUpdates();
+    }
+
+    private void updateConfig() {
+        ConfigUpdater configUpdater = new ConfigUpdater(this);
+        configUpdater.checkAndUpdateConfig();
+        reloadConfig();
+    }
+
+    private void initializeLanguageSystem() {
         new LanguageUpdater(this, List.of("en_US"));
         languageManager = new LanguageManager(this);
         messageService = new MessageService(this, languageManager);
         languageManager = new LanguageManager(this,
                 LanguageManager.LanguageFileType.MESSAGES
         );
+    }
+
+    @Override
+    public void onEnable() {
+        checkSmartSpawnerAPI();
+        checkPluginUpdates();
+        updateConfig();
+        initializeLanguageSystem();
     }
 
     @Override
